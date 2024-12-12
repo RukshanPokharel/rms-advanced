747b677ff9b6a23833522f779e5ccffe03ff1824
RukshanPokharel <rukshanpokharel@gmail.com>
2024-12-12 19:28:35 +0100

    database seeded
    intitial seeding of postgres sql neon database from vercel

---
+++ app/lib/placeholder-data.ts
@@ -0,0 +1,45 @@
+// This file contains placeholder data that you'll be replacing with real data in the Data Fetching chapter:
+// https://nextjs.org/learn/dashboard-app/fetching-data
+const users = [
+  {
+    id: "410544b2-4001-4271-9855-fec4b6a6442a",
+    name: "User",
+    email: "user@nextmail.com",
+    password: "123456",
+  },
+];
+
+const bookings = [
+  {
+    booking_id: "d6e15727-9fe1-4961-8c5b-ea44a9bd81aa",
+    noOfGuest: 5,
+    dateOfBooking: "2024-12-25",
+    booking_time: "18:00",
+  },
+  {
+    booking_id: "3958dc9e-712f-4377-85e9-fec4b6a6442a",
+    noOfGuest: 10,
+    dateOfBooking: "2024-12-25",
+    booking_time: "17:00",
+  },
+  {
+    booking_id: "3958dc9e-742f-4377-85e9-fec4b6a6442a",
+    noOfGuest: 2,
+    dateOfBooking: "2024-12-26",
+    booking_time: "19:00",
+  },
+  {
+    booking_id: "76d65c26-f784-44a2-ac19-586678f7c2f2",
+    noOfGuest: 4,
+    dateOfBooking: "2024-12-27",
+    booking_time: "20:00",
+  },
+  {
+    booking_id: "CC27C14A-0ACF-4F4A-A6C9-D45682C144B9",
+    noOfGuest: 9,
+    dateOfBooking: "2024-12-28",
+    booking_time: "17:00",
+  },
+];
+
+export { users, bookings };
